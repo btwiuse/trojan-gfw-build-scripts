97fb535b1c80b2698a73826abf34b524ff9ee8df
navigaid <navigaid@gmail.com>
2020-04-22 18:27:18 +0800

    update igniter build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+SCRIPT_DIR="$(dirname $(realpath $0))"
+TROJAN="${SCRIPT_DIR}"
+IGNITER="${SCRIPT_DIR}/igniter"
+cd "${IGNITER}"
+cp ../app/build.gradle app
+cp ../gradle.properties .
 ./gradlew assembleRelease
+find . -name '*.apk' | xargs -L1 realpath
