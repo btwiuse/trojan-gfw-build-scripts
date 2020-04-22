4b773226e39a092d8c0e7a32fa80a50db1ea2004
navigaid <navigaid@gmail.com>
2020-04-22 18:31:08 +0800

    move patch files to igniter-patch

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ SCRIPT_DIR="$(dirname $(realpath $0))"
 TROJAN="${SCRIPT_DIR}"
 IGNITER="${SCRIPT_DIR}/igniter"
 cd "${IGNITER}"
-cp ../app/build.gradle app
-cp ../gradle.properties .
+cp -r ../igniter-patch/* .
 ./gradlew assembleRelease
 find . -name '*.apk' | xargs -L1 realpath
